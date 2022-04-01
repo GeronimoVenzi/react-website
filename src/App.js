2c93f987b727f4a7770026106e5f785c8103a4ed
geronimovenzi <geronimovenzi@live.com.ar>
2022-03-31 22:49:30 -0300

    se agregan otros componentes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Food from './components/food/Food'
+import Footer from './components/footer/Footer'
 
 //Import images
-import Craft1 from './assets/craft1.jpg'
 import Craft2 from './assets/craft2.jpg'
-import Burguer from './assets/burguer.jpg'
+import Craft1 from './assets/craft1.jpg'
+import Burger from './assets/burger.jpg'
 import Chicken from './assets/chicken.jpg'
-import Footer from './assets/footer.jpg'
 import HeroBg from './assets/hero-bg.jpg'
-import Hero from './assets/hero.jpg'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Food />
+      <Food bgImg={Craft1} bgImg2={Craft2} />
+      <Food bgImg={Chicken} />
+      <Food bgImg={Burger} />
+      <Food bgImg={HeroBg} />
+      <Footer />
     </>
   );
 }
